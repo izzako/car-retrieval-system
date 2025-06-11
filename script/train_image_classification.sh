0811a36b998ac7568f8b84de595d5a50f425d1c5
izzako <musa.izzanardi@gmail.com>
2025-06-11 11:01:28 +0700

    update path

--- script/train_image_classification.sh
+++ script/train_image_classification.sh
@@ -1,2 +1,2 @@
-mkdir -p ./.cache
-"/projectnb/llamagrp/izzan/env/bin/python" source_code/finetune_car_type_classifier.py > "./.cache/train.log"
+mkdir -p ./logs
+"/projectnb/llamagrp/izzan/env/bin/python" source_code/finetune_car_type_classifier.py > "./logs/train.logs"
